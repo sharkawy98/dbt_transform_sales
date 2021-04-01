072d28a618b18a06a2a68485e172ef1395daf8f1
Ahmed Elsharkawy <sharkawyy98@gmail.com>
2021-04-01 22:21:56 +0000

    refactor fct_orders & dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -8,33 +8,35 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
-customer_orders as (
-
+customer_orders as(
     select
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as orders_count
-    from orders
+        count(order_id) as orders_count,
+        sum(price) as total_spent,
+    from
+        {{ ref('fct_orders') }}
     group by 1
 ),
 
 final as (
     select
-        l.customer_id,
-        l.first_name,
-        l.last_name,
-        r.first_order_date,
-        r.most_recent_order_date,
-        coalesce(r.orders_count, 0) as orders_count
-    from customers l
+        a.customer_id,
+        a.first_name,
+        a.last_name,
+        b.first_order_date,
+        b.most_recent_order_date,
+        coalesce(b.orders_count, 0) as orders_count,
+        coalesce(b.total_spent, 0) as total_spent
+    from customers a
+    
+    -- left join to include customers who didn't buy anything
     left join 
-        customer_orders r using (customer_id)
-    order by
-        r.orders_count desc
+        customer_orders b using(customer_id)
 )
 
 select * from final
